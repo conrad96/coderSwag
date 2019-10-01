89fe60ac513158cdb1e9ba2565538d4f0b60e5ea
conrad96 <conrad.mugisha@gmail.com>
2019-10-01 16:27:47 +0300

    displayed categories with resp titles and names

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -4,19 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.example.coderswag.R
+import com.example.coderswag.adapters.CategoryAdapter
 import com.example.coderswag.model.Category
 import com.example.coderswag.services.Dataservice
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, Dataservice.category)
+        adapter = CategoryAdapter(this, Dataservice.category)
 
         categoryListView.adapter = adapter
     }
