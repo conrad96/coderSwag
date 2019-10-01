a347dde75f55ecc7864bd3c15670e79f0957a244
conrad96 <conrad.mugisha@gmail.com>
2019-10-01 14:32:13 +0300

    added adapter to link list view to Dataservices obj

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.coderswag.R
+import com.example.coderswag.model.Category
+import com.example.coderswag.services.Dataservice
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, Dataservice.category)
+
+        categoryListView.adapter = adapter
     }
 }
