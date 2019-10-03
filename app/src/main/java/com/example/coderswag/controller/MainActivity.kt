9a39eb199c8a515c1f9abcc2c9f880f3befe3d5b
conrad96 <conrad.mugisha@gmail.com>
2019-10-03 09:51:02 +0300

    added clicklisteners on category list items

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.coderswag.R
 import com.example.coderswag.adapters.CategoryAdapter
-import com.example.coderswag.model.Category
 import com.example.coderswag.services.Dataservice
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,5 +19,11 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this, Dataservice.category)
 
         categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener{
+           adapterView, view, i, l ->
+            val category = Dataservice.category[i]
+            Toast.makeText(this, "You tapped the ${category.title} tab", Toast.LENGTH_SHORT).show()
+        }
     }
 }
