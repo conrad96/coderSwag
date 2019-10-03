ce2f18d380e99aa2cd25cdce00a743f8814395e6
conrad96 <conrad.mugisha@gmail.com>
2019-10-03 11:02:40 +0300

    Added recycle view adapter

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -2,28 +2,31 @@ package com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coderswag.R
 import com.example.coderswag.adapters.CategoryAdapter
+import com.example.coderswag.adapters.CategoryRecycleAdapter
 import com.example.coderswag.services.Dataservice
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, Dataservice.category)
+        adapter = CategoryRecycleAdapter(this, Dataservice.category)
 
         categoryListView.adapter = adapter
 
-        categoryListView.setOnItemClickListener{
-           adapterView, view, i, l ->
-            val category = Dataservice.category[i]
-            Toast.makeText(this, "You tapped the ${category.title} tab", Toast.LENGTH_SHORT).show()
-        }
+        val layoutManager = LinearLayoutManager(this)
+
+        categoryListView.layoutManager = layoutManager
+
+        categoryListView.setHasFixedSize(true)
     }
 }
