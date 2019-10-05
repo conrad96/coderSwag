b705bc84102f343121ee8c212ce1293311e51507
conrad96 <conrad.mugisha@gmail.com>
2019-10-05 12:58:29 +0300

    edited a few constraints on product structure

--- app/src/main/java/com/example/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/ProductsActivity.kt
@@ -2,15 +2,25 @@ package com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.coderswag.R
+import com.example.coderswag.adapters.ProductAdapter
+import com.example.coderswag.services.Dataservice
 import com.example.coderswag.utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
         val category = intent.getStringExtra(EXTRA_CATEGORY)
-        println(category)
+
+        adapter = ProductAdapter(this, Dataservice.getProducts(category))
+
+        val layoutManager = GridLayoutManager(this, 2)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
     }
 }
