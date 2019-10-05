efdd17d8255a65faa198258380b429968613727b
conrad96 <conrad.mugisha@gmail.com>
2019-10-05 11:49:41 +0300

    added code to product adapter

--- app/src/main/java/com/example/coderswag/services/Dataservice.kt
+++ app/src/main/java/com/example/coderswag/services/Dataservice.kt
@@ -12,25 +12,25 @@ object Dataservice {
     )
 
     val hats = listOf(
-        Product("Devslopes Graphic Beanie", "hat01", "$18.0"),
-        Product("Devslopes Hat Black", "hat02", "$20.0"),
-        Product("Devslopes Hat White", "hat03", "$18.0"),
-        Product("Devslopes Hat Snapback", "hat04", "$22.0")
+        Product("Devslopes Graphic Beanie", "hat1", "$18.0"),
+        Product("Devslopes Hat Black", "hat2", "$20.0"),
+        Product("Devslopes Hat White", "hat3", "$18.0"),
+        Product("Devslopes Hat Snapback", "hat4", "$22.0")
     )
 
     val hoodies = listOf(
-        Product("Devslopes Hoodie Gray", "hoodie01", "$28.0"),
-        Product("Devslopes Hoodie Red", "hoodie02", "$32.0"),
-        Product("Devslopes Gray Hoodie", "hoodie03", "$28.0"),
-        Product("Devslopes Black Hoodie", "hoodie04", "$32.0")
+        Product("Devslopes Hoodie Gray", "hoodie1", "$28.0"),
+        Product("Devslopes Hoodie Red", "hoodie2", "$32.0"),
+        Product("Devslopes Gray Hoodie", "hoodie3", "$28.0"),
+        Product("Devslopes Black Hoodie", "hoodie4", "$32.0")
     )
 
     val shirts = listOf(
-        Product("Devslopes Shirt Black", "shirt01", "$18.0"),
-        Product("Devslopes Badge Light Gray", "shirt02", "$20.0"),
-        Product("Devslopes Logo Shirt Red", "shirt03", "$22.0"),
-        Product("Devslopes Hustle", "shirt04", "$18.0"),
-        Product("Kickflip Studios", "shirt05", "$19.0")
+        Product("Devslopes Shirt Black", "shirt1", "$18.0"),
+        Product("Devslopes Badge Light Gray", "shirt2", "$20.0"),
+        Product("Devslopes Logo Shirt Red", "shirt3", "$22.0"),
+        Product("Devslopes Hustle", "shirt4", "$18.0"),
+        Product("Kickflip Studios", "shirt5", "$19.0")
     )
 
     val digitalGood = listOf<Product>()
